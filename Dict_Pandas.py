ac89e44ea1a46136a29a6909198d39e74fd6c498
whitelaw-josh <whitelaw.josh@yahoo.com>
2019-07-11 17:30:19 -0400

    Panda dataframes.

--- Dict_Pandas.py
+++ Dict_Pandas.py
@@ -56,5 +56,45 @@ data = { 'capital':'rome', 'population':59.83 }
 # Add data to europe under key 'italy'
 europe['italy'] = data
 
-# Print europe
-print(europe)
+# Pre-defined lists
+names = ['United States', 'Australia', 'Japan', 'India', 'Russia', 'Morocco', 'Egypt']
+dr =  [True, False, False, False, True, True, True]
+cpc = [809, 731, 588, 18, 200, 70, 45]
+
+# Import pandas as pd
+import pandas as pd
+
+# Create dictionary my_dict with three key:value pairs: my_dict
+my_dict = { 'country' : names, 'drives_right' : dr, 'cars_per_cap' : cpc }
+
+# Build a DataFrame cars from my_dict: cars
+cars = pd.DataFrame(my_dict)
+
+# Print cars
+print(cars)
+
+# Build cars DataFrame
+dict = { 'country':names, 'drives_right':dr, 'cars_per_cap':cpc }
+cars = pd.DataFrame(dict)
+print(cars)
+
+# Definition of row_labels
+row_labels = ['US', 'AUS', 'JAP', 'IN', 'RU', 'MOR', 'EG']
+
+# Specify row labels of cars
+cars.index = row_labels
+
+# Print cars again
+print(cars)
+
+# Import the cars.csv data: cars
+cars = pd.read_csv('cars.csv')
+
+# Print out cars
+print(cars)
+
+# Fix import by including index_col
+cars = pd.read_csv('cars.csv', index_col=0)
+
+# Print out cars
+print(cars)
