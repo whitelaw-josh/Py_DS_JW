b58ba556a55562de25eff932d7ecf6e16a77db81
whitelaw-josh <whitelaw.josh@yahoo.com>
2019-07-11 17:42:29 -0400

    Accessing dataframes.

--- Dict_Pandas.py
+++ Dict_Pandas.py
@@ -94,7 +94,43 @@ cars = pd.read_csv('cars.csv')
 print(cars)
 
 # Fix import by including index_col
-cars = pd.read_csv('cars.csv', index_col=0)
+cars = pd.read_csv('cars.csv', index_col = 0)
 
 # Print out cars
 print(cars)
+
+# Print out country column as Pandas Series
+print(cars["country"])
+
+# Print out country column as Pandas DataFrame
+print(cars[["country"]])
+
+# Print out DataFrame with country and drives_right columns
+print(cars[["country", "drives_right"]])
+
+# Print out first 3 observations
+print(cars[0:3])
+
+# Print out fourth, fifth and sixth observation
+print(cars[3:])
+
+# Print out observation for Japan
+print(cars.loc['JAP'])
+
+# Print out observations for Australia and Egypt
+print(cars.loc[['AUS', 'EG']])
+
+# Print out drives_right value of Morocco
+print(cars.loc[['MOR'], 'drives_right'])
+
+# Print sub-DataFrame
+print(cars.loc[['RU', 'MOR'], ['country', 'drives_right']])
+
+# Print out drives_right column as Series
+print(cars.loc[:, 'drives_right'])
+
+# Print out drives_right column as DataFrame
+print(cars.loc[:, ['drives_right']])
+
+# Print out cars_per_cap and drives_right as DataFrame
+print(cars.loc[:, ['cars_per_cap', 'drives_right']])
