fca67e3ffadd509946697ffb9e63d9ca5017a026
whitelaw-josh <whitelaw.josh@yahoo.com>
2019-08-06 17:31:24 -0400

    Dataset selections of panda series.

--- Adv_Basics_Comp_Logic.py
+++ Adv_Basics_Comp_Logic.py
@@ -76,3 +76,27 @@ elif area > 10 :
     print("medium size, nice!")
 else :
     print("pretty small.")
+
+# Import cars data
+import pandas as pd
+cars = pd.read_csv('cars.csv', index_col = 0)
+
+# Convert code to a one-liner
+sel = cars[cars['drives_right']]
+# dr = cars['drives_right']
+# sel = cars[dr]
+
+# Print sel
+print(sel)
+
+# Create car_maniac: observations that have a cars_per_cap over 500
+car_maniac = cars[cars["cars_per_cap"] > 500]
+
+# Print car_maniac
+print(car_maniac)
+
+# Create medium: observations with cars_per_cap between 100 and 500
+medium = cars[np.logical_and(cars["cars_per_cap"] > 100, cars["cars_per_cap"] < 500 )]
+
+# Print medium
+print(medium)
