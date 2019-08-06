303b097eefdc9f14bd93b43738e297957f80be85
whitelaw-josh <whitelaw.josh@yahoo.com>
2019-08-06 15:00:40 -0400

    If, elseif, else blocks.

--- Adv_Basics_Comp_Logic.py
+++ Adv_Basics_Comp_Logic.py
@@ -56,3 +56,23 @@ print(np.logical_or(my_house > 18.5, my_house < 10))
 
 # Both my_house and your_house smaller than 11
 print(np.logical_and(my_house < 11, your_house < 11))
+
+# Define variables
+room = "bed"
+area = 14.0
+
+# if-elif-else construct for room
+if room == "kit" :
+    print("looking around in the kitchen.")
+elif room == "bed":
+    print("looking around in the bedroom.")
+else :
+    print("looking around elsewhere.")
+
+# if-elif-else construct for area
+if area > 15 :
+    print("big place!")
+elif area > 10 :
+    print("medium size, nice!")
+else :
+    print("pretty small.")
