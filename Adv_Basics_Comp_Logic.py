819ad741b3c66bfa463d5d7d5b132d2e75acb507
whitelaw-josh <whitelaw.josh@yahoo.com>
2019-08-06 14:51:15 -0400

    Advanced basics with comparison operatores.

---
+++ Adv_Basics_Comp_Logic.py
@@ -0,0 +1,58 @@
+# Comparison of booleans
+True == False
+
+# Comparison of integers
+-5 * 15 != 75
+
+# Comparison of strings
+"pyscript" == "PyScript"
+
+# Compare a boolean with an integer
+True == 1
+
+# Comparison of integers
+x = -3 * 6
+print (x >= -10)
+
+# Comparison of strings
+y = "test"
+print("test" <= y)
+
+# Comparison of booleans
+True > False
+
+# Create arrays
+import numpy as np
+my_house = np.array([18.0, 20.0, 10.75, 9.50])
+your_house = np.array([14.0, 24.0, 14.25, 9.0])
+
+# my_house greater than or equal to 18
+print(my_house >= 18)
+
+# my_house less than your_house
+print(my_house < your_house)
+
+# Define variables
+my_kitchen = 18.0
+your_kitchen = 14.0
+
+# my_kitchen bigger than 10 and smaller than 18?
+print(my_kitchen > 10 and my_kitchen < 18)
+
+# my_kitchen smaller than 14 or bigger than 17?
+print(my_kitchen < 14 or my_kitchen > 17)
+
+# Double my_kitchen smaller than triple your_kitchen?
+print(2 * my_kitchen < 3 * your_kitchen)
+
+x = 8
+y = 9
+not(not(x < 3) and not(y > 14 or y > 10))
+
+not((x >= 3) and (y <= 14 and y <=10)))
+
+# my_house greater than 18.5 or smaller than 10
+print(np.logical_or(my_house > 18.5, my_house < 10))
+
+# Both my_house and your_house smaller than 11
+print(np.logical_and(my_house < 11, your_house < 11))
